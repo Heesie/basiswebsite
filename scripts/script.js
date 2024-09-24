e9ae27a4bdbbc8ac0e97a449c40339aadd637cad
Heesie <ljvanhees@outlook.com>
2024-09-24 14:48:27 +0200

    aanpassingen

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,8 @@ window.addEventListener('scroll', function() {
   if (scrollTop === 0) {
     // De gebruiker is helemaal bovenaan de pagina: maak de navigatiebalk weer transparant
     navbar.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    navbar.style.color = "white"; // Optioneel: tekstkleur aanpassen
+
   } else if (scrollTop > lastScrollTop) {
     // De gebruiker scrolt naar beneden: verberg de navigatiebalk
     navbar.style.top = "-80px";
@@ -20,8 +22,22 @@ window.addEventListener('scroll', function() {
     // De gebruiker scrolt omhoog: toon de navigatiebalk en maak deze wit
     navbar.style.top = "0";
     navbar.style.backgroundColor = "rgba(255, 255, 255, 1)"; // Wit maken bij omhoog scrollen
-  }
+    navbar.style.color = "black"; // Optioneel: tekstkleur aanpassen
 
+  }
   lastScrollTop = scrollTop; // Update de laatste scrollpositie
+  
 });
+
+        // Functie die naar het einde van de pagina scrollt
+        function scrollToBottom() {
+          window.scrollTo(0, document.body.scrollHeight);
+      }
+
+      // Event listener voor het toetsenbord
+      document.addEventListener('keydown', function(event) {
+          if (event.key === 'Escape') { 
+              scrollToBottom();
+          }
+      });
 
